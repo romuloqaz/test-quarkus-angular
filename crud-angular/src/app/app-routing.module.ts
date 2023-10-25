89c1ac876067704543ecec949e97eb7e9e52603e
Romulo Pereira <romulo.pereiracz@gmail.com>
2023-10-25 17:01:22 -0300

    updating angular projetct

---
+++ crud-angular/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'courses',
+  },
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./courses/courses.module').then((c) => c.CoursesModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
